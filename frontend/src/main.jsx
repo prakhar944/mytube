731bfb1d363c50502a9be5b2a52e1914a2bdcd7a
Prakhar <prakharshrivastava109@gmail.com>
2026-09-11 18:46:01 +0530

    update dependencies to React 19.3.0 and related packages; refactor main.jsx for improved imports and structure; add initial VideoCard component; clean up index.css for better styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom/client"
+import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx"
-// import "./index.css"
 
 
-ReactDom.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
+
+createRoot(document.getElementById("root")).render(
+      <StrictMode>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </React.StrictMode>
+      </StrictMode>
 )
